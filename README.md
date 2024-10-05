1b6ab893126837c2f05ff9c779af5eeaffd47645
BENAISSA8SAMIR <git@stackblitz.com>
2024-10-05 18:07:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# find-index-in-array-object-with-matching-element-ca9yqp
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/BENAISSA8SAMIR/find-index-in-array-object-with-matching-element-ca9yqp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  object = [
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG1_1549426981035.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549426985017,
+      "id": "1",
+      "mt": "022e12e4-b0c8-4c7e-9a5c-ac0896780016",
+      "text": "Chapters\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG2_1549429131749.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549429133969,
+      "id": "2",
+      "mt": "af113aec-06f9-4eb5-8aee-8c5cf6459f01",
+      "text": "Chapters\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG3_1549510419782.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549510432541,
+      "id": "3",
+      "mt": "45054bcd-5370-42de-9d5d-3ba44ca0bfa9",
+      "text": "When the name was placed there, Mr. James Dillingham Young\nwas being paid $30 a week. Now, when he was being paid only $20 a\nweek, the name seemed too long and important. It should perhaps have\nbeen “Mr. James D. Young.” But when Mr. James Dillingham Young\nentered the furnished rooms, his name became very short indeed. Mrs.\nJames Dillingham Young put her arms warmly about him and called\nhim “Jim.\" You have already met her. She is Della.\nDella finished her crying and cleaned the marks of it from her face.\nShe stood by the window and looked out with no interest. Tomorrow\nwould be Christmas Day, and she had only $1.87 with which to buy\nJim a gift. She had put aside as much as she could for months, with this\nresult. Twenty dollars a week is not much. Everything had cost more\nthan she had expected. It always happened like that.\nOnly $ 1.87 to buy a gift for Jim. Her Jim. She had had many happy\nhours planning something nice for him. Something nearly good enough.\nSomething almost worth the honor of belonging to Jim.\nThere was a looking-glass between the windows of the room. Per-\nhaps you have seen the kind of looking-glass that is placed in $8 fur-\nnished rooms. It was very narrow. A person could see only a little of\nhimself at a time. However, if he was very thin and moved very quickly,\nhe might be able to get a good view of himself. Della, being quite thin,\nhad mastered this art.\nSuddenly she turned from the window and stood before the glass.\nHer eyes were shining brightly, but her face had lost its color. Quickly\nshe pulled down her hair and let it fall to its complete length.\nThe James Dillingham Youngs were very proud of two things which\nthey owned. One thing was Jim's gold watch. It had once belonged to\nhis father. And, long ago, it had belonged to his father's father. The\nother thing was Della's hair.\nIf a queen had lived in the rooms near theirs, Della would have\nwashed and dried her hair where the queen could see it. Della knew\nher hair was more beautiful than any queen's jewels and gifts.\nIf a king had lived in the same house, with all his riches, Jim would\nhave looked at his watch every time they met. Jim knew that no king\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG4_1549514644521.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549514651866,
+      "id": "4",
+      "mt": "5b378c18-14d7-4f5b-a78e-a42ed83f37b4",
+      "text": "When the name was placed there, Mr. James Dillingham Young\nwas being paid $30 a week. Now, when he was being paid only $20 a\nweek, the name seemed too long and important. It should perhaps have\nbeen “Mr. James D. Young.” But when Mr. James Dillingham Young\nentered the furnished rooms, his name became very short indeed. Mrs.\nJames Dillingham Young put her arms warmly about him and called\nhim “Jim.\" You have already met her. She is Della.\nDella finished her crying and cleaned the marks of it from her face.\nShe stood by the window and looked out with no interest. Tomorrow\nwould be Christmas Day, and she had only $1.87 with which to buy\nJim a gift. She had put aside as much as she could for months, with this\nresult. Twenty dollars a week is not much. Everything had cost more\nthan she had expected. It always happened like that.\nOnly $ 1.87 to buy a gift for Jim. Her Jim. She had had many happy\nhours planning something nice for him. Something nearly good enough.\nSomething almost worth the honor of belonging to Jim.\nThere was a looking-glass between the windows of the room. Per-\nhaps you have seen the kind of looking-glass that is placed in $8 fur-\nnished rooms. It was very narrow. A person could see only a little of\nhimself at a time. However, if he was very thin and moved very quickly,\nhe might be able to get a good view of himself. Della, being quite thin,\nhad mastered this art.\nSuddenly she turned from the window and stood before the glass.\nHer eyes were shining brightly, but her face had lost its color. Quickly\nshe pulled down her hair and let it fall to its complete length.\nThe James Dillingham Youngs were very proud of two things which\nthey owned. One thing was Jim's gold watch. It had once belonged to\nhis father. And, long ago, it had belonged to his father's father. The\nother thing was Della's hair.\nIf a queen had lived in the rooms near theirs, Della would have\nwashed and dried her hair where the queen could see it. Della knew\nher hair was more beautiful than any queen's jewels and gifts.\nIf a king had lived in the same house, with all his riches, Jim would\nhave looked at his watch every time they met. Jim knew that no king\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG5_1549515919221.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549515926157,
+      "id": "5",
+      "mt": "c4c10eeb-c41c-4ffc-abcf-a5df2f377b62",
+      "text": "When the name was placed there, Mr. James Dillingham Young\nwas being paid $30 a week. Now, when he was being paid only $20 a\nweek, the name seemed too long and important. It should perhaps have\nbeen “Mr. James D. Young.” But when Mr. James Dillingham Young\nentered the furnished rooms, his name became very short indeed. Mrs.\nJames Dillingham Young put her arms warmly about him and called\nhim “Jim.\" You have already met her. She is Della.\nDella finished her crying and cleaned the marks of it from her face.\nShe stood by the window and looked out with no interest. Tomorrow\nwould be Christmas Day, and she had only $1.87 with which to buy\nJim a gift. She had put aside as much as she could for months, with this\nresult. Twenty dollars a week is not much. Everything had cost more\nthan she had expected. It always happened like that.\nOnly $ 1.87 to buy a gift for Jim. Her Jim. She had had many happy\nhours planning something nice for him. Something nearly good enough.\nSomething almost worth the honor of belonging to Jim.\nThere was a looking-glass between the windows of the room. Per-\nhaps you have seen the kind of looking-glass that is placed in $8 fur-\nnished rooms. It was very narrow. A person could see only a little of\nhimself at a time. However, if he was very thin and moved very quickly,\nhe might be able to get a good view of himself. Della, being quite thin,\nhad mastered this art.\nSuddenly she turned from the window and stood before the glass.\nHer eyes were shining brightly, but her face had lost its color. Quickly\nshe pulled down her hair and let it fall to its complete length.\nThe James Dillingham Youngs were very proud of two things which\nthey owned. One thing was Jim's gold watch. It had once belonged to\nhis father. And, long ago, it had belonged to his father's father. The\nother thing was Della's hair.\nIf a queen had lived in the rooms near theirs, Della would have\nwashed and dried her hair where the queen could see it. Della knew\nher hair was more beautiful than any queen's jewels and gifts.\nIf a king had lived in the same house, with all his riches, Jim would\nhave looked at his watch every time they met. Jim knew that no king\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG6_1549515931479.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549515933800,
+      "id": "6",
+      "mt": "71528e73-8ab9-4c3e-8d22-23f372cad581",
+      "text": "Chapters\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG7_1549516020151.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549516027408,
+      "id": "7",
+      "mt": "4061bf47-35ee-4a41-a976-3e2933ff7e0a",
+      "text": "When the name was placed there, Mr. James Dillingham Young\nwas being paid $30 a week. Now, when he was being paid only $20 a\nweek, the name seemed too long and important. It should perhaps have\nbeen “Mr. James D. Young.” But when Mr. James Dillingham Young\nentered the furnished rooms, his name became very short indeed. Mrs.\nJames Dillingham Young put her arms warmly about him and called\nhim “Jim.\" You have already met her. She is Della.\nDella finished her crying and cleaned the marks of it from her face.\nShe stood by the window and looked out with no interest. Tomorrow\nwould be Christmas Day, and she had only $1.87 with which to buy\nJim a gift. She had put aside as much as she could for months, with this\nresult. Twenty dollars a week is not much. Everything had cost more\nthan she had expected. It always happened like that.\nOnly $ 1.87 to buy a gift for Jim. Her Jim. She had had many happy\nhours planning something nice for him. Something nearly good enough.\nSomething almost worth the honor of belonging to Jim.\nThere was a looking-glass between the windows of the room. Per-\nhaps you have seen the kind of looking-glass that is placed in $8 fur-\nnished rooms. It was very narrow. A person could see only a little of\nhimself at a time. However, if he was very thin and moved very quickly,\nhe might be able to get a good view of himself. Della, being quite thin,\nhad mastered this art.\nSuddenly she turned from the window and stood before the glass.\nHer eyes were shining brightly, but her face had lost its color. Quickly\nshe pulled down her hair and let it fall to its complete length.\nThe James Dillingham Youngs were very proud of two things which\nthey owned. One thing was Jim's gold watch. It had once belonged to\nhis father. And, long ago, it had belonged to his father's father. The\nother thing was Della's hair.\nIf a queen had lived in the rooms near theirs, Della would have\nwashed and dried her hair where the queen could see it. Della knew\nher hair was more beautiful than any queen's jewels and gifts.\nIf a king had lived in the same house, with all his riches, Jim would\nhave looked at his watch every time they met. Jim knew that no king\n"
+    },
+    {
+      "audioPath": "audio/BKndAXVNvLD00SkruVhD97_CH1_PG8_1549516027478.mp3",
+      "bookId": "ndAXVNvLD00SkruVhD97",
+      "chapterId": "1",
+      "date": 1549516029266,
+      "id": "8",
+      "mt": "1b5514e1-a007-405b-904d-1fab92903641",
+      "text": "Chapters\n"
+    }
+  ]
+
+  index = this.object.findIndex(o => o.id === '3');
+
+  result = this.object[this.index+1];
+  // result = this.object.filter(o => {
+  //   return o.id == this.index.toString();
+  // })
+
+}
